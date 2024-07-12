ea55d94e3e349f34f32a50bc703fdd23b7fbda8c
caigq <2961509073@qq.com>
2024-07-12 10:50:37 +0800

    完成将可用列表按照表格打印

--- src/handler/list.rs
+++ src/handler/list.rs
@@ -1,27 +1,73 @@
+use prettytable::{Cell, Row, Table};
+use reqwest::Response;
+use std::collections::BTreeMap;
+
+type VersionMap = BTreeMap<String, Vec<String>>;
+
 pub fn handle_list() {
-    println!("List handler");
     let _ = get_available_versions_list();
 }
 
 #[tokio::main]
 async fn get_available_versions_list() -> Result<(), reqwest::Error> {
     let url = "https://nodejs.org/dist/index.json";
-
-    let response = reqwest::get(url).await?;
-
+    let mut version_map: VersionMap = BTreeMap::new();
+    let response: Response = reqwest::get(url).await?;
     if response.status().is_success() {
         let versions: serde_json::Value = response.json().await?;
         if let Some(versions_array) = versions.as_array() {
             for version in versions_array {
-                // println!("Versions: {:?}", version);
-                if let Some(version_str) = version["version"].as_str() {
-                    println!("Version: {}", version_str);
+                // 如果为boolean 则跳过本次循环
+                if version["lts"].is_boolean() {
+                    continue;
+                }
+                if version_map.contains_key(&version["lts"].to_string()) {
+                    version_map
+                        .get_mut(&version["lts"].to_string())
+                        .unwrap()
+                        .push(version["version"].to_string());
+                } else {
+                    version_map.insert(
+                        version["lts"].to_string(),
+                        vec![version["version"].to_string()],
+                    );
                 }
             }
         }
+
+        print_table_stdout(&version_map);
     } else {
         eprintln!("Failed to fetch versions: {}", response.status());
     }
-
     Ok(())
+}
+
+fn print_table_stdout(version_map: &VersionMap) {
+    let mut table = Table::new();
+
+    let header: Row = version_map
+        .keys()
+        .map(|key| Cell::new(&key.replace("\"", "")))
+        .collect();
+
+    table.add_row(header);
+
+    let mut max_len = 0;
+
+    for (k, _) in version_map.iter() {
+        if k.len() > max_len {
+            max_len = k.len();
+        }
+    }
+    for index in 0..max_len {
+        let mut row: Vec<Cell> = vec![];
+        for (_, (_, v)) in version_map.iter().enumerate() {
+            if v[index].is_empty() {
+                continue;
+            }
+            row.push(Cell::new(&v[index].replace("\"", "")));
+        }
+        table.add_row(Row::new(row));
+    }
+    table.printstd();
 }
