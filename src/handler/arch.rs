7adea592e8e01e9b4b88d7f89b128aee0db0faf5
caigq <2961509073@qq.com>
2024-07-15 10:43:42 +0800

    完成arch命令显示系统架构信息

--- src/handler/arch.rs
+++ src/handler/arch.rs
@@ -1,3 +1,36 @@
+use sysinfo::{self, System};
+
+#[derive(Debug)]
+pub struct SystemInfo {
+    pub sys_name: String,
+    pub sys_kernel_version: String,
+    pub sys_os_version: String,
+    pub sys_host_name: String,
+    pub sys_cpu_arch: String,
+    pub sys_long_os_version: String,
+}
+
+impl SystemInfo {
+    pub fn new() -> Self {
+        Self {
+            sys_name: System::name().unwrap(),
+            sys_kernel_version: System::kernel_version().unwrap(),
+            sys_os_version: System::os_version().unwrap(),
+            sys_host_name: System::host_name().unwrap(),
+            sys_cpu_arch: System::cpu_arch().unwrap(),
+            sys_long_os_version: System::long_os_version().unwrap(),
+        }
+    }
+}
+
 pub fn handle_arch() {
-    println!("Arch handler");
+    if sysinfo::IS_SUPPORTED_SYSTEM {
+        let sys_info = SystemInfo::new();
+        println!(
+            "System Info: {} {}",
+            sys_info.sys_long_os_version, sys_info.sys_cpu_arch
+        );
+    } else {
+        println!("This OS isn't supported (yet?).");
+    }
 }
