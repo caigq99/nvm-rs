f00354467fbfd676c3e2b1cf92a70f71b17290c9
caigq <2961509073@qq.com>
2024-07-24 16:17:20 +0800

    完成windows平台默认配置路径的下载和解压缩

--- src/handler/install.rs
+++ src/handler/install.rs
@@ -1,15 +1,9 @@
+use crate::utils::config::get_default_node_path;
 use crate::utils::download::{get_download_url, get_suffix};
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
-use std::{
-    fmt::Write,
-    fs, io,
-    path::{Path, PathBuf},
-};
-use tokio::{
-    fs::{self as tokiofs, File},
-    io::AsyncWriteExt,
-};
+use std::{fmt::Write, fs, io, path::PathBuf};
+use tokio::{fs::File, io::AsyncWriteExt};
 use zip::ZipArchive;
 
 #[tokio::main]
@@ -18,8 +12,16 @@ pub async fn handle_install(version: String) -> Result<()> {
     if !version.starts_with("v") {
         version.insert(0, 'v');
     }
-    //如果该目录存在，说明已经下载过了，直接退出
-    match tokiofs::try_exists(&version).await? {
+
+    let node_dir = get_default_node_path();
+
+    if !node_dir.exists() {
+        fs::create_dir_all(node_dir.as_path())?;
+    }
+
+    let node_file = node_dir.as_path().join(&version);
+
+    match node_file.try_exists()? {
         true => {
             println!("version {} already exists", version);
             Ok(())
@@ -28,7 +30,24 @@ pub async fn handle_install(version: String) -> Result<()> {
             let download_url = get_download_url(&version);
             println!("download url: {}", download_url);
             download_file(&download_url).await?;
+            // #[cfg(windows)]
             unzip_file(&format!("{}{}", &version, get_suffix())).await?;
+
+            let old_name = node_dir
+                .join(
+                    &download_url
+                        .split("/")
+                        .last()
+                        .unwrap()
+                        .replace(get_suffix(), ""),
+                )
+                .to_str()
+                .unwrap()
+                .to_string();
+            let new_name = node_dir.join(&version).to_str().unwrap().to_string();
+            rename_directory(&old_name, &new_name)?;
+            // #[cfg(not(windows))]
+            // untar_file();
             Ok(())
         }
     }
@@ -45,15 +64,15 @@ async fn download_file(url: &str) -> Result<()> {
         .filter(|x| x.starts_with("v"))
         .last()
         .unwrap();
-    println!("total size: {}", total_size);
-    println!("filename: {}", filename);
-    // 创建临时压缩包和文件夹
-    let mut file = File::create(format!("{}{}", filename, get_suffix())).await?;
-    // let _ = fs::create_dir(filename).await?;
+    let file_path = get_default_node_path().join(format!("{}{}", filename, get_suffix()));
+    // 创建临时压缩包
+    println!("file_path1: {}", file_path.display());
+    let mut file = File::create(&file_path).await?;
+
     // 创建进度条
     let progress = ProgressBar::new(total_size);
     // 设置进度条样式
-    progress .set_style(ProgressStyle::with_template("Starting download \n {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta}) \n{msg}")
+    progress.set_style(ProgressStyle::with_template("Starting download \n {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta}) \n{msg}")
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-"));
@@ -69,25 +88,27 @@ async fn download_file(url: &str) -> Result<()> {
 }
 
 // 在windows系统，解压zip包
+#[warn(dead_code)]
 async fn unzip_file(filename: &str) -> Result<()> {
-    let name = filename.replace(".zip", "");
-    let file = fs::File::open(filename).unwrap();
-    let extract_path = PathBuf::from(&name);
-    println!("extract path: {:?}", extract_path);
+    let node_dir = get_default_node_path();
+    let file = fs::File::open(node_dir.join(&filename)).unwrap();
     let mut archive = ZipArchive::new(file).unwrap();
     let total_files = archive.len() as u64;
     // 创建进度条
     let progress = ProgressBar::new(total_files);
     // 设置进度条样式
-    progress .set_style(ProgressStyle::with_template("Starting extract \n {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta}) \n{msg}")
+    progress.set_style(ProgressStyle::with_template("Starting extract \n {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta}) \n{msg}")
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-"));
     let mut extract_num = 0;
     for i in 0..archive.len() {
+        if i == 0 {
+            continue;
+        }
         let mut file = archive.by_index(i).unwrap();
         let outpath = match file.enclosed_name() {
-            Some(path) => path,
+            Some(path) => PathBuf::from(&node_dir).join(&path),
             None => continue,
         };
         if file.is_dir() {
@@ -109,4 +130,10 @@ async fn unzip_file(filename: &str) -> Result<()> {
 }
 
 // 在linux,mac系统，解压tar包
+#[warn(dead_code)]
 fn untar_file() {}
+
+fn rename_directory(old_directory: &str, new_directory: &str) -> Result<()> {
+    fs::rename(old_directory, new_directory)?;
+    Ok(())
+}
